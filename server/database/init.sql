21fb91ce7b691bd9a3a52c1f73c7b25f26f1fb98
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2024-03-14 12:08:40 +0000

    This is a beautiful example of how to run the use the database

--- server/database/init.sql
+++ server/database/init.sql
@@ -1,2 +1,12 @@
 SELECT 'CREATE DATABASE dss'
     WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dss')\gexec
+
+DROP TABLE users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- server/index.js
+++ server/index.js
@@ -3,11 +3,24 @@ const app = express()
 const port = 8000
 const db = require('./database/index.js')
 
+app.use(express.json())
+
 app.get('/', async (req, res) => {
     const result = await db.query('SELECT NOW()').catch(e => console.error(e.stack))
     res.json(result?.rows)
 })
 
+app.post('/create', async (req, res) => {
+    const {
+        id,
+        username,
+        password,
+        email
+    } = req.body
+    // https://node-postgres.com/guides/project-structure
+    const result = await db.query('INSERT INTO users (id, username, password, email) VALUES ($1, $2, $3, $4)', [id, username, password, email]).catch(e => console.error(e.stack))
+    res.json(result?.rows)
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
